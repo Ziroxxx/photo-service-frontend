14bbfe0cbfbd279d1dec85862a63f81e2000e012
Andrew <vipklopa@mail.ru>
2026-03-19 20:17:21 +0300

    basename

--- competition-photo-front/src/app/router.tsx
+++ competition-photo-front/src/app/router.tsx
@@ -3,17 +3,22 @@ import AuthPage from '../pages/AuthPage';
 import CompetitionsPage from '../pages/CompetitionsPage';
 import CompetitionDetailsPage from '../pages/CompetitionDetailsPage';
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <AuthPage />,
+    },
+    {
+      path: '/competitions',
+      element: <CompetitionsPage />,
+    },
+    {
+      path: '/competitions/:id',
+      element: <CompetitionDetailsPage />,
+    },
+  ],
   {
-    path: '/',
-    element: <AuthPage />,
-  },
-  {
-    path: '/competitions',
-    element: <CompetitionsPage />,
-  },
-  {
-    path: '/competitions/:id',
-    element: <CompetitionDetailsPage />,
-  },
-]);
+    basename: '/photo-service-frontend',
+  }
+);
