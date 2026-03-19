1ccf35d0d3cc2e8198809e09cf8506a5f998653c
Andrew <vipklopa@mail.ru>
2026-03-19 20:26:35 +0300

    router check 1

--- competition-photo-front/src/app/router.tsx
+++ competition-photo-front/src/app/router.tsx
@@ -1,21 +1,19 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { createHashRouter } from 'react-router-dom';
 import AuthPage from '../pages/AuthPage';
 import CompetitionsPage from '../pages/CompetitionsPage';
 import CompetitionDetailsPage from '../pages/CompetitionDetailsPage';
 
-export const router = createBrowserRouter(
-  [
-    {
-      path: '/',
-      element: <AuthPage />,
-    },
-    {
-      path: '/competitions',
-      element: <CompetitionsPage />,
-    },
-    {
-      path: '/competitions/:id',
-      element: <CompetitionDetailsPage />,
-    },
-  ],
-);
+export const router = createHashRouter([
+  {
+    path: '/',
+    element: <AuthPage />,
+  },
+  {
+    path: '/competitions',
+    element: <CompetitionsPage />,
+  },
+  {
+    path: '/competitions/:id',
+    element: <CompetitionDetailsPage />,
+  },
+]);
--- competition-photo-front/src/main.tsx
+++ competition-photo-front/src/main.tsx
@@ -3,12 +3,9 @@ import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/global.css';
 import App from './App';
-import { HashRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
-  </React.StrictMode>,
+    <App />
+  </React.StrictMode>
 );
